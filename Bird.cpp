c08cf7863d22472e23d27d96e015962b42ac730b
jmbuhr <jmbuhr@noctrl.edu>
2022-03-03 19:39:51 -0800

    Created main.cpp, Bird.cpp, and Bird.h

---
+++ Bird.cpp
@@ -0,0 +1,7 @@
+// Joshua M Buhr
+// CSCE306; Spring 2022
+// Portfolio Assignment
+
+// Code Summary:
+
+#include "Bird.h"
---
+++ Bird.h
@@ -0,0 +1,14 @@
+// Joshua M Buhr
+// CSCE306; Spring 2022
+// Portfolio Assignment
+
+// Code Summary:
+
+#ifdef CSCE306_PORTFOLIO_BIRD_H
+#define CSCE306_PORTFOLIO_BIRD_H
+
+class Bird {
+    
+};
+
+#endif //CSCE306_PORTFOLIO_BIRD_H
